7d50c18d012d94cdd9160c44468c6aff8b67b3d8
diegotosato <tosato1998@gmail.com>
2025-10-22 15:10:07 +0200

    snack 1 - print in console the array elements
    uso both for cycle and method forEach

--- snack-1/script.js
+++ snack-1/script.js
@@ -8,3 +8,18 @@ initialize for cycle
 isolate single element
 log in console the element
 */
+
+
+//with for
+for (let i = 0; i < names.length; i++) {
+    const thisName = names[i];
+    console.log(thisName);
+    
+}
+
+
+
+//with forEach
+names.forEach((name)=>{
+    console.log(name);
+})
