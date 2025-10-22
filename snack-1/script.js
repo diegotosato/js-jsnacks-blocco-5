9b0d3f7bc0b9622bc995a0151ee5720cbc18a299
diegotosato <tosato1998@gmail.com>
2025-10-22 15:08:10 +0200

    snack 1 - write pseudo code

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,9 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+/*
+initialize for cycle
+isolate single element
+log in console the element
+*/
