49f55f3a0a03aac1b5f6b83f71d3ed7afffe2352
diegotosato <tosato1998@gmail.com>
2025-10-22 15:51:27 +0200

    snack 8 - write pseudo code

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,11 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+/*
+initialize for cycle
+isolate single object "class" key
+declare condition
+  - if key === 3C -> log key
+*/
+
