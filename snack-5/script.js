f6d236b9fe775b719cc52e6c894bcc47ac7c25b6
diegotosato <tosato1998@gmail.com>
2025-10-22 15:24:54 +0200

    snack 5 - write pseudo code

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,13 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+/*
+create empty array
+initialize for cycle
+isolate single number
+declare condition to find only even numbers (% 2 === 0)
+push into the empty array
+log new array
+*/
+
